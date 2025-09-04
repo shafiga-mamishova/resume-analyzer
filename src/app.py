1c3d7659e4e9ff3423f2773ee14339e47e63f49f
Shafiga Mamishova <shafiga.mamishova@student.kdg.be>
2025-09-04 20:06:05 +0400

    Extracting text from PDF file

--- src/app.py
+++ src/app.py
@@ -1,3 +1,18 @@
 # TODO: Streamlit for UI
 
 import streamlit as st
+import resume_parser as rp
+
+st.title("Resume Analyzer",)
+
+col1,col2=st.columns(2)
+
+with col1:
+    st.text_area("Job Description")
+
+with col2:
+    resume=st.file_uploader("Upload Your Resume", type=["pdf"])
+
+if resume is not None:
+    rp.parse(resume)
+    st.success("PDF processed!")
--- src/resume_parser.py
+++ src/resume_parser.py
@@ -1,1 +1,10 @@
 # TODO: User uploads PDF. Parse PDF as plain text
+
+import pypdf
+
+def parse(file):
+    reader=pypdf.PdfReader(file)
+    text=""
+    for page in reader.pages:
+        text += page.extract_text()
+    print(text)
